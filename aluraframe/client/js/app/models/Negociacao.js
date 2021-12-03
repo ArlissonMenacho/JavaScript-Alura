d6c52c9b4454398581d1687c4087920443f2474f
Arlisson Menacho <Arlissonmenacho@gmail.com>
2021-12-03 19:35:59 -0400

    Protegente o código contra modificação
    Nessa aula vimos que o freeze não soluciona 100% o problema de modificação externa dos valores, como por exemplo a propriedade date, os valores são congelados mas os dentro da propriedade seus métodos ainda continuam a interagir.

--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -1,9 +1,9 @@
 class Negociacao{
     constructor(data, quantidade, valor){
-        this._data = data;
+        this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
-        Object.freeze(this);
+        Object.freeze(this); 
     }
 
     get volume() {
@@ -11,7 +11,7 @@ class Negociacao{
     }
 
     get dia(){
-        return this._data;
+        return new Date(this._data.getTime()) ;
     }
 
     get quantidade(){
