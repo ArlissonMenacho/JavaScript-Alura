05815b434a78f1254307290bf22e86417e76f3c4
Arlisson Menacho <Arlissonmenacho@gmail.com>
2021-12-04 18:07:56 -0400

    Sprid, split e map
    Explicação de como utilizar cada uma das funções

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,15 +1,34 @@
 class NegociacaoController{
+
+    constructor(){
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
+
     adiciona(event){
         event.preventDefault();
-        
-        let $ = document.querySelector.bind(document);
+        console.log(typeof(this._inputData.value));
+        console.log(this._inputData.value);
 
-        let inputData = $('#data');
-        let inputQuantidade = $('#quantidade');
-        let inputValor = $('#valor');
+        let data = new Date(...this._inputData.value
+            .split('-')
+            .map((item,indice) =>{
+                if(indice==1){
+                    return item - 1;
+                }
+                return item;
+            }));
 
-        console.log(inputData.value);
-        console.log(inputQuantidade.value);
-        console.log(inputValor.value);
+        console.log(data);
+        console.log(typeof(data));
+
+      let negociacao = new Negociacao(
+          data,
+          this._inputQuantidade.value,
+          this._inputValor.value
+      );
+      console.log(negociacao);
     }
 }
